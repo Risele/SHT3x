251be05160d1c840300954b6870a3a4909604956
Danil <risele@mail.ru>
2017-09-06 14:36:25 +0300

    Update Calibration.ino
    minor changes in description

--- examples/Calibration/Calibration.ino
+++ examples/Calibration/Calibration.ino
@@ -1,12 +1,13 @@
 /*
  * If you found that your sensor is not well-calibrated, you can overcalibrate it (with linear function).
  * Calibration translates values recieved from your SHT3x to data that your reference sensor recievs under the same conditions.
- * You can overcalibrated both temperature and relative humidity.
+ * You can overcalibrated both temperature and relative humidity independently.
  * Note, that temperature calibration use CELSIUS for overcalculate data:
  *  FIRST, the temperature converts from sensor byte data to Celsius (check SHT3x::UpdateData),
  *  SECOND, temperature scales up in respect to calibration.
  *  THIRD, temperature converts from Celsius to your scale
- *  You can check SHT3x.cpp, SHT3x::UpdateData and SHT3x::GetTemperature
+ *  You can check SHT3x.cpp, SHT3x::UpdateData and SHT3x::GetTemperature for more information.
+ *  Example for non-celsius scales located at the row 71.
  * The same is for relative humidity.
  * 
  * There are two ways for overcalibration: by Factors and by Points.
@@ -24,6 +25,7 @@
  *  };
  * 
  * and
+ * 
  * struct CalibrationPoints
  * {
  *  float First;
@@ -43,19 +45,19 @@ SHT3x Sensor_Far(0x45); //This operates in Fahrenheit
 void setup() {
   SHT3x::CalibrationPoints HumidityReference; //Points from reference sensor
   SHT3x::CalibrationPoints HumiditySHT; //Points from your SHT3x sensor
-  HumidityReference.First = 0.; //If you really could set those ambient conditions, please, call me 8-800-355-...
+  HumidityReference.First = 0.; //If you really could achieve those ambient conditions, please, call me 8-800-355-...
   HumidityReference.Second = 100.;
   HumiditySHT.First = 5.3;
   HumiditySHT.Second = 97.;
-  Sensor.SetRelHumidityCalibrationPoints(HumiditySHT, HumidityReference);
+  Sensor.SetRelHumidutyCalibrationPoints(HumiditySHT, HumidityReference);
 
   SHT3x::CalibrationFactors TemperatureCalibration;
   TemperatureCalibration.Factor = -1.;
-  TemperatureCalibration.Shift = -1000;
+  TemperatureCalibration.Shift = -1000.;
   /*And than use Kelvin, muhaha!
   Ok, that was a joke. Here are real values from my expeirence (Celsius):
   my SHT30 and trusted instrument have shown 
-  (temperature conditions was similar, we tested it over humidity changes):
+  (temperature conditions were similar, we tested it over humidity changes):
   SHT30: 23.6 ; 25.1;
   TrIns: 22.3 ; 23.7;
   So, the overcalibraion equation is:
@@ -75,10 +77,10 @@ void setup() {
    * 167  180
    * I have to convert it to Celsius:
    * SHT  Ref //Celsius
-   * 0   1.7
-   * 10  11.1
-   * 75  82.2
-   * For this particular dataset overcalibration is the next (linear approximation by Microsoft Excel):
+   * 0    1.7
+   * 10   11.1
+   * 75   82.2
+   * For this particular dataset overcalibration is the following (linear approximation by Microsoft Excel):
    */
   TemperatureCalibration.Factor = 1.0807; //We have already set calibration for Celsius sensor,
   TemperatureCalibration.Shift  = 1.0467; //so, there is no need for a new variable
